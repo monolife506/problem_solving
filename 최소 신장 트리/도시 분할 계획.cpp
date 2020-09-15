667d7ca272bbab9e21ef7060cfb7a3ee43254520
An Yong Mo <aym506@outlook.com>
2020-09-15 23:13:30 +0900

    Solved at 20200915

---
+++ 최소 신장 트리/도시 분할 계획.cpp
@@ -0,0 +1,74 @@
+// 1647번: 도시 분할 계획
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int root[100001];
+
+struct Edge
+{
+    int u;
+    int v;
+    int w;
+
+    Edge(int u, int v, int w)
+    {
+        this->u = u;
+        this->v = v;
+        this->w = w;
+    }
+
+    bool operator<(const Edge &U) const { return this->w < U.w; }
+};
+
+int find(int N)
+{
+    if (root[N] == 0)
+        return N;
+    return root[N] = find(root[N]);
+}
+
+bool merge(int U, int V)
+{
+    U = find(U);
+    V = find(V);
+    if (U != V)
+    {
+        root[U] = V;
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+    vector<Edge> edges;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+    for (size_t i = 0; i < M; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back(Edge(u, v, w));
+    }
+    sort(edges.begin(), edges.end());
+
+    int edgeCnt = 0;
+    int totalWeight = 0;
+    for (size_t i = 0; edgeCnt < N - 2; i++)
+    {
+        if (!merge(edges[i].u, edges[i].v))
+        {
+            edgeCnt++;
+            totalWeight += edges[i].w;
+        }
+    }
+
+    cout << totalWeight;
+}
