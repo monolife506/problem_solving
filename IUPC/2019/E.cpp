ba1ff5fdf8b6dac5cff5acfdcf348c731128a50f
An Yong Mo <aym506@outlook.com>
2020-12-22 02:21:13 +0900

    Solved at 20201221

--- IUPC/2019/E.cpp
+++ IUPC/2019/E.cpp
@@ -2,57 +2,74 @@
 #include <queue>
 using namespace std;
 
-struct P
+int arr[1000][1000];
+bool visited[1000][1000];
+char buffer[1001];
+
+struct Qitem
 {
     int X;
     int Y;
     int L;
     int R;
-
-    P() {}
-    P(int x, int y, int l, int r) : X(x), Y(y), L(l), R(r) {}
+    Qitem(int _X, int _Y, int _L, int _R) : X(_X), Y(_Y), L(_L), R(_R) {}
 };
 
-int N, M, L, R;
-int arr[1001][1001];
-
-char buffer[1001];
-int startX, startY;
-bool visited[1001][1001];
-
-int bfs()
+int bfs(int N, int M, int lCnt, int rCnt, int startX, int startY)
 {
     static int dx[4] = {1, -1, 0, 0};
     static int dy[4] = {0, 0, 1, -1};
 
-    int ans = 0;
-    queue<P> Q;
-    Q.push(P(startX, startY, 0, 0));
+    int ans = 1;
+    queue<Qitem> Q;
+    Q.push(Qitem(startX, startY, lCnt, rCnt));
     visited[startX][startY] = true;
 
     while (!Q.empty())
     {
-        P cur = Q.front();
+        Qitem cur = Q.front();
         Q.pop();
-
-        if (cur.L <= L && cur.R <= R)
-            ans++;
 
         for (size_t i = 0; i < 4; i++)
         {
-            int nextX = cur.X + dx[i];
-            int nextY = cur.Y + dy[i];
-            if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= M)
-                continue;
-            if (!visited[nextX][nextY] && arr[nextX][nextY] == 0)
+            int X = cur.X;
+            int Y = cur.Y + dy[i];
+
+            // 상하 탐색
+            if (i == 0 || i == 1)
             {
-                visited[nextX][nextY] = true;
-                if (i == 2)
-                    Q.push(P(nextX, nextY, cur.L, cur.R + 1));
-                else if (i == 3)
-                    Q.push(P(nextX, nextY, cur.L + 1, cur.R));
-                else
-                    Q.push(P(nextX, nextY, cur.L, cur.R));
+                while (X >= 0 && X < N)
+                {
+                    X += dx[i];
+                    if (X < 0 || X >= N || arr[X][Y] == 1)
+                        break;
+                    if (!visited[X][Y])
+                    {
+                        ans++;
+                        visited[X][Y] = true;
+                        Q.push(Qitem(X, Y, cur.L, cur.R));
+                    }
+                }
+            }
+            else // 좌우 탐색
+            {
+                if (Y < 0 || Y >= M || arr[X][Y] == 1)
+                    continue;
+                if (!visited[X][Y])
+                {
+                    if (i == 2 && cur.R > 0)
+                    {
+                        ans++;
+                        visited[X][Y] = true;
+                        Q.push(Qitem(X, Y, cur.L, cur.R - 1));
+                    }
+                    else if (i == 3 && cur.L > 0)
+                    {
+                        ans++;
+                        visited[X][Y] = true;
+                        Q.push(Qitem(X, Y, cur.L - 1, cur.R));
+                    }
+                }
             }
         }
     }
@@ -66,6 +83,7 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
+    int N, M, L, R, startX, startY;
     cin >> N >> M >> L >> R;
     for (size_t i = 0; i < N; i++)
     {
@@ -81,5 +99,5 @@ int main()
         }
     }
 
-    cout << bfs();
+    cout << bfs(N, M, L, R, startX, startY);
 }
